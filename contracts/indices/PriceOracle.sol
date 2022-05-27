051e2fa12d6c6486a6892be833c79c6280a33f3e
knucklesuganda <testaddressss43@gmail.com>
2022-05-27 10:22:35 +0200

    Temporary save with new price quoter

--- contracts/indices/PriceOracle.sol
+++ contracts/indices/PriceOracle.sol
@@ -25,6 +25,7 @@ import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 import '@uniswap/v3-core/contracts/libraries/TickMath.sol';
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/IQuoterV2.sol";
+import "@uniswap/v3-periphery/contracts/libraries/OracleLibrary.sol";
 import "@uniswap/v3-core/contracts/libraries/FixedPoint96.sol";
 import "@uniswap/v3-core/contracts/libraries/FullMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
@@ -35,34 +36,18 @@ contract PriceOracle {
     using SafeMath for uint256;
     IUniswapV3Factory private factory;
     address baseToken;
+    address WETH;
 
     constructor(){
         factory = IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
         baseToken = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+        WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     }
 
-    function getPrice2(address secondToken, uint24 poolFee) external view returns (uint) {
-        IUniswapV3Pool uniswapv3Pool = IUniswapV3Pool(factory.getPool(baseToken, secondToken, poolFee));
-        // (uint sqrtPriceX96, , , , , , ) = uniswapv3Pool.slot0();
-        
-        uint32[] memory secondAgos = new uint32[](2);
-        secondAgos[0] = 1;
-        secondAgos[1] = 0;
-
-        (int56[] memory tickCumulatives, ) = uniswapv3Pool.observe(secondAgos);
-
-        int56 tickCumulativesDiff = tickCumulatives[1] - tickCumulatives[0];
-        uint56 period = uint56(secondAgos[0]-secondAgos[1]);
-
-        int56 timeWeightedAverageTick = (tickCumulativesDiff / -int56(period));
-
-        uint8 decimalToken0 = ERC20(uniswapv3Pool.token0()).decimals();
-        uint160 sqrtRatioX96 = TickMath.getSqrtRatioAtTick(int24(timeWeightedAverageTick));
-        uint256 ratioX192 = uint256(sqrtRatioX96) * sqrtRatioX96;
-
-        // sqrtRatioX96 ** 2 / 2 ** 192 = price
-        return uint(sqrtRatioX96).mul(uint(sqrtRatioX96)).mul(1 ether).div(2 ** 192);
+    function getPrice(address secondToken) external view returns (uint) {
+        IUniswapV3Pool pool = IUniswapV3Pool(factory.getPool(baseToken, secondToken, 3000));
+        (, int24 tick, , , , , ) = pool.slot0();
+        return OracleLibrary.getQuoteAtTick(tick, 1 ether, secondToken, baseToken);
     }
 
-    function getPrice(address a) pure public returns(uint){ return 100; }
 }
